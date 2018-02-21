8a41c653979b21b944822ab3298d8df1f02e85c0
Lauren Gleeson <laurengleeson1@gmail.com>
2018-02-21 14:46:36 +0000

    New class that handles all file operations and then is called in the main application

--- src/bankexercise/BankApplication.java
+++ src/bankexercise/BankApplication.java
@@ -46,6 +46,11 @@ public class BankApplication extends JFrame {
 	private String [] images = {"first.png", "prev.png", "next.png", "last.png"};
 	private JButton[] imageButtons = new JButton[images.length];
 	
+	static String fileToSaveAs = "";
+	private static RandomAccessFile input; 
+	private static RandomAccessFile output;
+	private FileHelper fileHelper;
+	
 	
 	
 	public BankApplication() {
@@ -54,6 +59,7 @@ public class BankApplication extends JFrame {
 		
 		int currentItem;
 		initComponents();
+		fileHelper = new FileHelper(fileToSaveAs, input, output);
 	}
 	
 	public void initComponents() {
@@ -312,7 +318,8 @@ public class BankApplication extends JFrame {
 		
 		fileMenuItems.get("Open File").addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				readFile();
+				//readFile();
+				fileHelper.readFile(table, fc);
 				currentItem=0;
 				while(!table.containsKey(currentItem)){
 					currentItem++;
@@ -323,13 +330,13 @@ public class BankApplication extends JFrame {
 		
 		fileMenuItems.get("Save File").addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				writeFile();
+				fileHelper.writeFile(table, fc);
 			}
 		});
 		
 		fileMenuItems.get("Save As").addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				saveFileAs();
+				fileHelper.saveFileAs(table,fc);
 			}
 		});
 		
@@ -338,7 +345,7 @@ public class BankApplication extends JFrame {
 				
 				int answer = JOptionPane.showConfirmDialog(BankApplication.this, "Do you want to save before quitting?");
 				if (answer == JOptionPane.YES_OPTION) {
-					saveFileAs();
+					fileHelper.saveFileAs(table, fc);
 					dispose();
 				}
 				else if(answer == JOptionPane.NO_OPTION)
@@ -508,200 +515,8 @@ public class BankApplication extends JFrame {
 		});
 	}
 	
-	private static RandomAccessFile input;
-	private static RandomAccessFile output;
-	private static final int NUMBER_RECORDS = 100;
-
 	
-	public static void openFileRead()
-	   {
-		
-		table.clear();
-			
-		fc = new JFileChooser();
-		int returnVal = fc.showOpenDialog(null);
-		 
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            File file = fc.getSelectedFile();
-
-        } else {
-                }
-
-			
-		      try // open file
-		      {
-		    	  if(fc.getSelectedFile()!=null)
-		    		  input = new RandomAccessFile( fc.getSelectedFile(), "r" );
-		      } // end try
-		      catch ( IOException ioException )
-		      {
-		    	  JOptionPane.showMessageDialog(null, "File Does Not Exist.");
-		      } // end catch
-			
-	   } // end method openFile
 	
-	static String fileToSaveAs = "";
-	
-	public static void openFileWrite()
-	   {
-		if(fileToSaveAs!=""){
-	      try // open file
-	      {
-	         output = new RandomAccessFile( fileToSaveAs, "rw" );
-	         JOptionPane.showMessageDialog(null, "Accounts saved to " + fileToSaveAs);
-	      } // end try
-	      catch ( IOException ioException )
-	      {
-	    	  JOptionPane.showMessageDialog(null, "File does not exist.");
-	      } // end catch
-		}
-		else
-			saveToFileAs();
-	   }
-	
-	public static void saveToFileAs()
-	   {
-		
-		fc = new JFileChooser();
-		
-		 int returnVal = fc.showSaveDialog(null);
-         if (returnVal == JFileChooser.APPROVE_OPTION) {
-             File file = fc.getSelectedFile();
-           
-             fileToSaveAs = file.getName();
-             JOptionPane.showMessageDialog(null, "Accounts saved to " + file.getName());
-         } else {
-             JOptionPane.showMessageDialog(null, "Save cancelled by user");
-         }
-        
-     	    
-	         try {
-	        	 if(fc.getSelectedFile()==null){
-	        		 JOptionPane.showMessageDialog(null, "Cancelled");
-	        	 }
-	        	 else
-	        		 output = new RandomAccessFile(fc.getSelectedFile(), "rw" );
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	      
-	      
-	     
-	   }
-	
-	public static void closeFile() 
-	   {
-	      try // close file and exit
-	      {
-	         if ( input != null )
-	            input.close();
-	      } // end try
-	      catch ( IOException ioException )
-	      {
-	         
-	    	  JOptionPane.showMessageDialog(null, "Error closing file.");//System.exit( 1 );
-	      } // end catch
-	   } // end method closeFile
-	
-	public static void readRecords()
-	   {
-	
-	      RandomAccessBankAccount record = new RandomAccessBankAccount();
-
-	      
-
-	      try // read a record and display
-	      {
-	         while ( true )
-	         {
-	            do
-	            {
-	            	if(input!=null)
-	            		record.read( input );
-	            } while ( record.getAccountID() == 0 );
-
-	       
-	            
-	            BankAccount ba = new BankAccount(record.getAccountID(), record.getAccountNumber(), record.getFirstName(),
-	                    record.getSurname(), record.getAccountType(), record.getBalance(), record.getOverdraft());
-	            
-	            
-	            Integer key = Integer.valueOf(ba.getAccountNumber().trim());
-			
-				int hash = (key%TABLE_SIZE);
-		
-				
-				while(table.containsKey(hash)){
-			
-					hash = hash+1;
-				}
-				
-	            table.put(hash, ba);
-		
-
-	         } // end while
-	      } // end try
-	      catch ( EOFException eofException ) // close file
-	      {
-	         return; // end of file was reached
-	      } // end catch
-	      catch ( IOException ioException )
-	      {
-	    	  JOptionPane.showMessageDialog(null, "Error reading file.");
-	         System.exit( 1 );
-	      } // end catch
-	   }
-	
-public static void saveToFile(){
-		
-	
-		RandomAccessBankAccount record = new RandomAccessBankAccount();
-	
-	      Scanner input = new Scanner( System.in );
-
-	      
-	      for (Map.Entry<Integer, BankAccount> entry : table.entrySet()) {
-			   record.setAccountID(entry.getValue().getAccountID());
-			   record.setAccountNumber(entry.getValue().getAccountNumber());
-			   record.setFirstName(entry.getValue().getFirstName());
-			   record.setSurname(entry.getValue().getSurname());
-			   record.setAccountType(entry.getValue().getAccountType());
-			   record.setBalance(entry.getValue().getBalance());
-			   record.setOverdraft(entry.getValue().getOverdraft());
-			   
-			   if(output!=null){
-			   
-			      try {
-						record.write( output );
-					} catch (IOException u) {
-						u.printStackTrace();
-					}
-			   }
-			   
-			}
-    	  
-	      
-	}
-
-	public static void writeFile(){
-		openFileWrite();
-		saveToFile();
-		//addRecords();
-		closeFile();
-	}
-	
-	public static void saveFileAs(){
-		saveToFileAs();
-		saveToFile();	
-		closeFile();
-	}
-	
-	public static void readFile(){
-	    openFileRead();
-	    readRecords();
-	    closeFile();		
-	}
 	
 	public void put(int key, BankAccount value){
 		int hash = (key%TABLE_SIZE);
@@ -723,70 +538,3 @@ public static void saveToFile(){
 	
 	
 }
-
-
-
-
-/*
-The task for your second assignment is to construct a system that will allow users to define a data structure representing a collection of records that can be displayed both by means of a dialog that can be scrolled through and by means of a table to give an overall view of the collection contents. 
-The user should be able to carry out tasks such as adding records to the collection, modifying the contents of records, and deleting records from the collection, as well as being able to save and retrieve the contents of the collection to and from external random access files.
-The records in the collection will represent bank account records with the following fields:
-
-AccountID (this will be an integer unique to a particular account and 
-will be automatically generated when a new account record is created)
-
-AccountNumber (this will be a string of eight digits and should also 
-be unique - you will need to check for this when creating a new record)
-
-Surname (this will be a string of length 20)
-
-FirstName (this will be a string of length 20)
-
-AccountType (this will have two possible options - "Current " and 
-"Deposit" - and again will be selected from a drop down list when 
-entering a record)
-
-Balance (this will a real number which will be initialised to 0.0 
-and can be increased or decreased by means of transactions)
-
-Overdraft (this will be a real number which will be initialised 
-to 0.0 but can be updated by means of a dialog - it only applies 
-to current accounts)
-
-You may consider whether you might need more than one class to deal with bank accounts.
-The system should be menu-driven, with the following menu options:
-
-Navigate: First, Last, Next, Previous, Find By Account Number 
-(allows you to find a record by account number entered via a 
-dialog box), Find By Surname (allows you to find a record by 
-surname entered via a dialog box),List All (displays the 
-contents of the collection as a dialog containing a JTable)
-
-Records: Create, Modify, Delete, Set Overdraft (this should 
-use a dialog to allow you to set or update the overdraft for 
-a current account), Set Interest Rate (this should allow you 
-to set the interest rate for deposit accounts by means of a 
-dialog)
-
-Transactions: Deposit, Withdraw (these should use dialogs which
-allow you to specify an account number and the amount to withdraw
-or deposit, and should check that a withdrawal would not cause
-the overdraft limit for a current account to be exceeded, or be 
-greater than the balance in a deposit account, before the balance 
-is updated), Calculate Interest (this calculates the interest rate 
-for all deposit accounts and updates the balances)
-
-File: Open, Save, Save As (these should use JFileChooser dialogs. 
-The random access file should be able to hold 25 records. The position 
-in which a record is stored and retrieved will be determined by its account 
-number by means of a hashing procedure, with a standard method being used when 
-dealing with possible hash collisions)
-
-Exit Application (this should make sure that the collection is saved - or that 
-the user is given the opportunity to save the collection - before the application closes)
-
-When presenting the results in a JTable for the List All option, the records should be sortable on all fields, but not editable (changing the records or adding and deleting records can only be done through the main dialog).
-For all menu options in your program, you should perform whatever validation, error-checking and exception-handling you consider to be necessary.
-The programs Person.java and PersonApplication.java (from OOSD2) and TableDemo.java may be of use to you in constructing your interfaces. The set of Java programs used to create, edit and modify random access files will also provide you with a basis for your submission.
-
-*/
